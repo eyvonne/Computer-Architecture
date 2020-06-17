feccd0bcec049223aae718b5f5138373ab3bbc67
eyvonne <eyvonne@users.noreply.github.com>
2020-06-17 09:21:51 -0700

    yesterdays mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,34 +16,55 @@ class CPU:
         self.MDR = 0
         self.flags = 0b00000000
 
-    def load(self):
+        self.cpuOps = {1: 'HLT',
+                       7: 'PRN',
+                       2: 'LDI'}
+
+    def load(self, file=None):
         """Load a program into memory."""
 
         address = 0
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
+        # go back to fix this.
+        if len(sys.argv) > 1:
+            file = sys.argv[1]
+            with open(file, 'r') as p:
+                program = [line.split()[0] for line in p if len(
+                    line.split()) > 0 and line.split()[0] != '#']
+        else:
+            program = [
+                # From print8.ls8
+                0b10000010,  # LDI R0,8
+                0b00000000,
+                0b00001000,
+                0b01000111,  # PRN R0
+                0b00000000,
+                0b00000001,  # HLT
+            ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.memory[address] = instruction
             address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
+        def ADD(reg_a, reg_b):
+            # its possible to come back and do these bitwise, but for now MVP
+            self.registers[reg_a] += self.registers[reg_b]
 
-        if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
-        else:
+        def SUB(reg_a, reg_b):
+            self.registers[reg_a] -= self.registers[reg_b]
+
+        def MULT(reg_a, reg_b):
+            # its possible to come back and only use the add and subtract functions
+            # for mult and div but again, MVP for now
+            self.registers[reg_a] *= self.registers[reg_b]
+        self.aluOps = {0: ADD,
+                       2: MULT}
+        try:
+            oper = self.aluOps[op]
+            oper(reg_a, reg_b)
+        except:
             raise Exception("Unsupported ALU operation")
 
     def trace(self):
@@ -62,7 +83,7 @@ class CPU:
         ), end='')
 
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+            print(" %02X" % self.registers[i], end='')
 
         print()
 
@@ -70,17 +91,38 @@ class CPU:
         """Run the CPU."""
         running = True
         while running:
-            bits = bin(self.ram_read(self.PC))[2:]
-            self.IR = bits[-4:]
-            if bits[2] == '1':
-                self.alu(bits)
-            if bits[0:1] == '10':
-                temp_b = self.ram_read(self.PC + 2)
-            if bits[0:1] == '01':
-                temp_a == self.ram_read(self.PC + 1)
+            # get the data out of the instruction
+            bits = int(self.ram_read(self.PC), 2)
+            self.IR = bits & int('00001111', 2)
+
+            temp_a = int(self.ram_read(self.PC + 1), 2) if (bits &
+                                                            int('11000000', 2)) >> 6 >= 1 else None
+            temp_b = int(self.ram_read(self.PC + 2), 2) if (bits &
+                                                            int('11000000', 2)) >> 6 >= 1 else None
+
+            # execute the instruction
+            if bits & int('00100000', 2):
+                # this part doesn't work yet, I was working ahead anyways.
+                self.alu(self.IR, temp_a, temp_b)
+                self.PC += 3 if temp_b is not None else 2
+            elif self.cpuOps[self.IR] == 'HLT':
+                running = False
+                break
+            elif self.cpuOps[self.IR] == 'LDI':
+                self.registers[temp_a] = temp_b
+                self.PC += 3
+            elif self.cpuOps[self.IR] == 'PRN':
+                print(self.registers[temp_a])
+                self.PC += 2
 
     def ram_read(self, address):
         return self.memory[address]
 
     def ram_write(self, address, value):
         self.memory[address] = value
+
+
+if __name__ == '__main__':
+    cpu = CPU()
+    cpu.load()
+    cpu.run()
